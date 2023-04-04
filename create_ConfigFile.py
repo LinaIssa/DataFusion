61d583b1f6a21d1d8f837ace389ea2d871aaca25
Lina Issa <linaissa@hotmail.fr>
2023-04-04 16:01:55 +0200

    updated the load fuction to make it more versatile

--- create_ConfigFile.py
+++ create_ConfigFile.py
@@ -5,7 +5,8 @@ Created on Tuesday December 13th
 @author: Lina Issa
 lina.issa@irap.omp.eu
 
-This python script creates a configuration file with the help of PyYAML python library. Once a configuration file created,you only need to load it before launching the fusion algorithm.
+This python script creates a configuration file with the help of PyYAML python library. Once a configuration file
+ created,you only need to load it before launching the fusion algorithm.
 Meaning of the parameters:
 
 - DataDir         : The directory in which the data files are stored
@@ -14,22 +15,34 @@ Meaning of the parameters:
 - PSF_MS          : ?? A .fits file used to get the h band    --> self-consistency problem
 - multi_image     : The path to the NIRCam image stored as a .fits file
 - hyper_image     : The path to the NIRSpec image stored as a .fits file
-- LM              : Spectral degradation operator for the multispectral image, constructed in  produce_HS_MS.py. Used in get_weights function. Need to change the name because of name conflict
-- LH              : Spectral degredation operator for the hyperspectral image. Used in the choose_subspace routine and in get_weights function
-- nr              : number of rows    ?
-- nc              : number of columns ?
-- fact_pad        : ??
+- LM              : Spectral degradation operator for the multispectral. The shape should be (number of bands in the
+multispectral image, number of bands in the hyperspectral image). Used in get_weights function. In the rows of LM are
+stored the transmission functions of the different filters.
+- LH              : Spectral degradationoperator for the hyperspectral image. The shape should be (number of bands in
+the hyperspectral image, number of bands in the hyperspectral image). It is a diagonal matrix with the spectral
+transmission function of NirSpec. Used in the choose_subspace routine and in get_weights function
+- nr              : number of pixels in the PSFs
+- nc              : number of pixels in the PSFs
+- fact_pad        : padding factor
 - FLUXCONV_NC     : ??
 - downsampling    : downsampling factor of the downsizing operator involved in the spatial deformation of the images (sous-échantillonnage). Should be an integer
 - lacp            : The spectral dimension of the reduced hyperspectral image as retrieved by a PCA.
 - mu              : the regularisation strength
+
 ************************************************* From Constants.py *****************************************************
 *       High spatial resolution image size with padding and padding factor                                              *
-*       Example :                                                                                                       *
+*       Examples :                                                                                                      *
 *       Original MS band size : 300x300                                                                                 *
-*       fact_pad : 41                                                                                                   *
+*       fact_pad : 41
+*       d        : 3
 *       nr = 300 + 2*fact_pad + 2                                                                                       *
-*       nc = 300 + 2*fact_pad + 2                                                                                       *
+*       nc = 300 + 2*fact_pad + 2
+
+*       Original MS band size : 90x90                                                                                   *
+*       fact_pad : 44
+*       d        : 6
+*       nr = 90 + 2*fact_pad + 2                                                                                        *
+*       nc = 90 + 2*fact_pad + 2
 *       Conditions (to chose fact_pad) : nr % d = 0; nc % d = 0; (2*fact_pad+2) % d = 0  and (fact_pad//d + 1)%2 =0     *
 *************************************************************************************************************************
 
@@ -67,6 +80,6 @@ data = {
     #    Dumping the dico into a config.yaml file   #
     #################################################
 
-with open('config.yaml', 'w+') as f:
+with open('fusion_config.yaml', 'w+') as f:
     f.write(dump(data))
 
