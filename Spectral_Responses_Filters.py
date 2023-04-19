88af3552c16e0361100f37b82ce2a32c1399022b
Lina Issa <linaissa@hotmail.fr>
2023-04-19 16:23:06 +0200

    added NIRSpec throughput simulation for the operator Lh

--- Spectral_Responses_Filters.py
+++ Spectral_Responses_Filters.py
@@ -3,10 +3,17 @@ from main       import load_config
 from astropy.io import fits
 from pandeia.engine.instrument_factory import InstrumentFactory
 
-"""
-In t
-"""
-def get_filters(filters_list : list, spectral_resampling : np.array, instrument_config: dict, ):
+'''
+@author: Lina Issa, adapted from Claire Guilloteau's FRHOMAGE algorithm.
+
+According to the instrumental filters selected in the configuration file, this script generates the spectral responses 
+for NIRCam and NIRSpec using pandeia engine. 
+
+'''
+# ----------------------------------------------------------------------------------------------------------------------
+#                                Get pce from pandeia engine for NIRSpec & NIRCam
+#----------------------------------------------------------------------------------------------------------------------
+def get_NIRCam_filters(filters_list : list, spectral_resampling : np.array, instrument_config: dict):
     '''
     Computes spectral degradation operator Lm of the instrument with unit convestion (mjy/arcsec^2 to e-)
     :param filters_list : the list of the NIRCam filters selected for the fusion. Defined in the fusion_config.yaml file
@@ -62,9 +69,48 @@ def get_filters(filters_list : list, spectral_resampling : np.array, instrument_
     Lm = cste_conv * Lm * spectral_resampling**(-1) * 1.5091905 * delt_lamb * exp_time * FLUXCONV_NC
 
     return Lm
+def get_NIRSpec_filters(filter : str, tablewave : np.ndarray) :
+    """
+    @author Lina Issa, inspired from the paper : "PDRs4all: NIRSpec simulation of integral field unit
+    spectroscopy of the Orion Bar photodissociation region" by Canin et al and submitted in 2022
+
+    Compute the combined throughput of entire telescope/instrument/detector system for NIRSpec instrument
+
+    : param filter    : the NIRSpec filter to consider for the fusion
+    : param tablewave : Wavelength vector to interpolate throughput onto. As defined in pandeia's instrument.py
+    : return          : system throughput as a function of wave
+
+    """
+
+    disperser_filter_combinations = {'f100lp': 'g140h', 'f170lp':'g235h', 'f290lp': 'g395h'}
+
+    if filter not in disperser_filter_combinations.keys():
+        raise ValueError(f'The provided filter {filter} is unknown. These are the expected filters: {disperser_filter_combinations.keys()}.')
+
+    disperser  = disperser_filter_combinations[filter]
+
+    obsmode  = {'instrument': 'nirspec',
+               'mode': 'ifu',
+               'disperser': disperser,
+               'filter': filter}
+
+    detector = {'readout_pattern': 'nrsrapid',
+                'nint': 1,
+                'ngroup': 5}
+
+    conf     = {'instrument': obsmode,
+                'detector': detector}
+
+    i = InstrumentFactory(config=conf)
+    pce = i.get_total_eff(tablewave)
+
+    return pce
+
 
 def get_pce(mode : str, config : dict, wave_resampling : np.array):
     '''
+    This function is tailored for NIRCam instrument
+
     :param mode : depends on the filter
     :param config :  a dict containing the context of the observation
     :param wave_resampling : the spectral resolution used for the upsampling of the NirCam spectral responses
@@ -84,17 +130,48 @@ def get_pce(mode : str, config : dict, wave_resampling : np.array):
     pce = i.get_total_eff(wave_resampling)
 
     return pce
-
+# ----------------------------------------------------------------------------------------------------------------------
+#                                          MAIN
+#----------------------------------------------------------------------------------------------------------------------
 def main(fusion_config : dict, instrument_config : dict) :
 
     NIRSpecData       = fits.getdata(fusion_config['hyper_image'])
+    tablewave         = fits.getdata(fusion_config['TableWave'])
     spectral_sampling = np.sort(NIRSpecData[:,0,0])
-    filters_list      = fusion_config['NIRCam_Filters']
+    spectral_scope    = fusion_config['Spectral_Scope']
+    NIRCam_Filters    = fusion_config['NIRCam_Filters']
+    NIRSpec_Filters   = fusion_config['NIRSpec_Filters']
+
+    if not isinstance(spectral_scope, list):
+        raise TypeError(f'The spectral scope provided is {type(spectral_scope)} while it should be a list')
+
+    if spectral_scope != [] :
+        wave1, wave2 = spectral_scope[0], spectral_scope[1]
+        if wave1 < spectral_sampling[1] and wave2 > spectral_sampling[1]:
+            a, b = np.where(spectral_sampling>wave1)[0][0], np.where(spectral_sampling<wave2[0][-1])
+            spectral_sampling = spectral_sampling[a:b]
 
 
-    Lm  = get_filters(filters_list, spectral_sampling, instrument_config)
-    hdu = fits.PrimaryHDU(Lm)
+    Lm_pce  = get_NIRCam_filters(NIRCam_Filters, spectral_sampling, instrument_config)
+    Lh_pce  = get_NIRSpec_filters(NIRSpec_Filters,tablewave)
+
+    Lh      = np.diag(
+        np.reshape(
+            Lh_pce,
+            (len(tablewave),)
+        )
+    )
+
+
+    hdu = fits.PrimaryHDU(Lm_pce)
     hdu.writeto("NirCam_SpectralResponse.fits", overwrite=True)
+
+    hdu = fits.PrimaryHDU(Lh)
+    hdu.writeto(f'NirSpec_Throughput_{NIRSpec_Filters}.fits', overwrite=True)
+
+# ----------------------------------------------------------------------------------------------------------------------
+#                                           Parameters loading
+#----------------------------------------------------------------------------------------------------------------------
 
 fusion_config     = load_config('fusion_config.yaml')
 instrument_config = load_config('observation_config.yaml', type = 'observation')
